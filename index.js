224a718528365b0744eb454ab6f680bf7ab9c1f5
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2024-08-27 16:35:02 +0000

    working on 2d array manipulation

--- index.js
+++ index.js
@@ -24,24 +24,64 @@ console.log(getFileExtension("www.google.com"));
 
 //String.charCodeAt String.fromCharCode
 
-const replaceWithNextChar = (anyOldString) => 
-
-anyOldString
-  .split("")
-  .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
-  .join("");
-
+const replaceWithNextChar = (anyOldString) =>
+  anyOldString
+    .split("")
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join("");
 
 console.log(replaceWithNextChar("abcd"));
 
 //4. Write a Javascript program to get the current date. Expected output: MM/DD/YYYY
 
 const getTheDate = (date = new Date()) => {
-  
-  const months = (date.getMonth() + 1);
-  const days = (date.getDate() - 1);  //why does it take a -1 for this to be right? tried other getdate methods and still it comes to the 22nd when today is the 23rd :(  const year = date.getFullYear();
-
-  return `${months}/${days}/${year}`
-}
-  
+  const months = date.getMonth() + 1;
+  const days = date.getDate() - 1; //why does it take a -1 for this to be right? tried other getdate methods and still it comes to the 22nd when today is the 23rd :(  const year = date.getFullYear();
+    const year = date.getFullYear();
+  return `${months}/${days}/${year}`;
+};
 console.log(getTheDate());
+
+//-----------------------------------2d array work ------------------------------------------------------------//
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+//Print the element at the second row, third column.
+
+console.log(
+  "1. Print the element at the second row, third column: " + matrix[1][2],
+);
+
+//loop through and print each element
+console.log("2. This shows each element in the matrix:");
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    console.log(matrix[i][j]);
+  }
+}
+//Change the value at the first row, second column to 10.
+
+matrix[0][1] = 10;
+console.log(matrix[0][1]);
+console.log("3. matrix[0][1] = " + matrix[0][1]);
+
+//Create a function to transpose the matrix (swap rows with columns). Did not do on your own!
+
+function transposeMatrix(matrix) {
+  let transposed = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    transposed[i] = [];
+    for (let j = 0; j < matrix.length; j++) {
+      transposed[i][j] = matrix[j][i];
+    }
+
+    return transposed;
+  }
+}
+let transposedMatrix = transposeMatrix(matrix);
+console.log(transposedMatrix)
